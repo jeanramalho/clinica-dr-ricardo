4ab1eb8be622377bc98eb77ce4cbca6debc673aa
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-09-16 15:25:11 -0300

    cria configuração tailwind para nova cor

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+
+  tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+              doura: '#b09c5e',
+          }
+        }
+      }
+    }
+  
+
 function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
