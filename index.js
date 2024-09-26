a5224b31983b021baaee3390273bf0d1bbe4f26b
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-09-25 22:46:50 -0300

    Cria a sessão de FAQ para o site˜

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ function menuOnClick() {
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+document.addEventListener('alpine:init', () => {
+  Alpine.store('accordion', {
+    tab: 0
+  });
+
+  Alpine.data('accordion', (idx) => ({
+    init() {
+      this.idx = idx;
+    },
+    idx: -1,
+    handleClick() {
+      this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx;
+    },
+    handleRotate() {
+      return this.$store.accordion.tab === this.idx ? 'rotate-180' : '';
+    },
+    handleToggle() {
+      return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : '';
+    }
+  }));
+})
